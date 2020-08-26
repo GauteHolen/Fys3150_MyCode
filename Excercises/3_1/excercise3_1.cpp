91dd9d70aea9abc9e601e2d33c08da2d2bb6182a
GauteHolen <gaute.ah@gmail.com>
2020-08-26 15:29:54 +0200

    setup of excercise 3.1 and compilation with launch.json and task.json

---
+++ Excercises/3_1/excercise3_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+double derivative(){
+    return 12345678;
+}
+
+
+int main(){
+
+    double result = derivative();
+
+    cout << result;
+
+    return 0;
+
+
+}
+
